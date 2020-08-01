1d1e96ccd177310762238a17e181b4c8a503dfc8
tobenary <tomer.ben.arye@gmail.com>
2020-08-01 23:00:50 +0300

    Update daily_english_send_telegram.py

--- daily_english_send_telegram.py
+++ daily_english_send_telegram.py
@@ -55,7 +55,7 @@ def print_time():
 api_id = os.environ['api_id']
 api_hash = os.environ['api_hash']
 session_token = os.environ['SESSION_TOKEN']
-schedule.every(1).minutes.do(print_time)
+# schedule.every(1).minutes.do(print_time)
 # schedule.every(2).minutes.do(send_message)
 schedule.every().day.at("07:30").do(send_message)
 
